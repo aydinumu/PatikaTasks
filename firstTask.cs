1574c83406d318a398d4e89b083d85333d556eb2
Umut Aydın <169164758+aydinumu@users.noreply.github.com>
2024-07-15 21:19:49 +0300

    Create firstTask.cs

---
+++ firstTask.cs
@@ -0,0 +1,12 @@
+using System;
+namespace konsolepp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Umut Aydın");
+            Console.WriteLine("Patika'yı önceden beri biliyordum. .NET eğitimi olunca yazılım dünyasına atılmak için iyi bir fırsat olabileceğini düşündüm ve bu yüzden buradayım.");
+        }
+    }
+}
