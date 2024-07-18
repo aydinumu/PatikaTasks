4e35e64204c3dd5ad19051b1eb2d26e9bd0eae79
Umut Aydın <169164758+aydinumu@users.noreply.github.com>
2024-07-18 13:22:58 +0300

    Create milyoner.cs

---
+++ milyoner.cs
@@ -0,0 +1,4 @@
+// Bu programda bir bilgi yarışması yapılıyor.
+// Kullanıcıya 3 soru soruyoruz , 3 şıktan oluşan. 2 veya üstü doğru cevap vermesi durumunda büyük ödül kazanıyor.
+// Eğer 2 yanlış cevap verirse kazanamayacağı için üçüncü soru sorulmuyor.
+// En sonda kullanıcıya sonucu veriliyor.
