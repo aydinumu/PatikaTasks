b80badc4f0eb46bccbe278c090bfe25f1675dd55
Umut Aydın <169164758+aydinumu@users.noreply.github.com>
2024-07-19 13:51:46 +0300

    Update yolArkadasi.cs

--- yolArkadasi.cs
+++ yolArkadasi.cs
@@ -15,7 +15,7 @@ namespace ConsoleApp
             string tripDecision; //Dowhile ile başladığım için önceden tanımlamalıydım.
             Console.WriteLine("HOŞGELDİNİZ !");
             do //Kullanıcı seçeneklerden birini seçmediği sürece 3 defa daha tekrarlıyoruz
-            {   
+            {
                 Console.WriteLine("3 günlük tatil tercihinize uygun yalnızca aşağıdaki 3 Adet seyehat paketimiz bulunmaktadır.");
                 Console.WriteLine("     Bodrum   : Kişi başı başlangıç fiyatı 4000TL");
                 Console.WriteLine("     Marmaris : Kişi başı Başlangıç fiyatı 3000TL");
@@ -25,7 +25,7 @@ namespace ConsoleApp
                 counter++;
             } while (tripDecision != "bodrum" && tripDecision != "marmaris" && tripDecision != "çeşme" && counter < 3);
 
-                if (counter == 3)
+            if (counter == 3)
             {   //Can sıkıntısından eklenen bir eklenti
                 Console.WriteLine("------------------------------------------");
                 Console.WriteLine("Yeterince trollediniz :D");
@@ -55,11 +55,10 @@ namespace ConsoleApp
                 Console.WriteLine("Bu kişi sayısı için hesap yapmamıza gerek yok zaten ilginiz için teşekkürler. Program kapatılıyor");
                 Environment.Exit(0);
             }
-                Console.WriteLine($"{tripDecision.ToUpperInvariant()} için {personCount} kişi ara toplam : {pricePerPerson * personCount}");
-
+            Console.WriteLine($"{tripDecision.ToUpperInvariant()} için {personCount} kişi ara toplam : {pricePerPerson * personCount}");
 
             Console.WriteLine("------------------------------------------");
-            switch (tripDecision) 
+            switch (tripDecision)
             {
                 case "bodrum":
                     Console.WriteLine("Bodrum, Türkiye'nin en popüler tatil beldelerinden biridir. Eşsiz koyları, beyaz evleri ve canlı gece hayatıyla ünlüdür. Bodrum Kalesi'ni ziyaret edebilir, antik tiyatroyu görebilir ve Barlar Sokağı'nda eğlenceli bir gece geçirebilirsiniz. Ayrıca, Bodrum’un kristal berraklığındaki denizinde yüzmeyi ve su sporları yapmayı unutmayın.");
@@ -76,11 +75,12 @@ namespace ConsoleApp
 
             string travelDecision = "0"; //Loop içinde çalıştırıyorum ki farklı cevap verirse tekrar tekrrar sorayım
             int counter2 = 0;
-            do {
+            do
+            {
                 Console.WriteLine($"1 - Kara yolu kişi başı gidiş dönüş 1500TL , {personCount} kişi toplam : {personCount * 1500}TL");
                 Console.WriteLine($"2 - Hava yolu kişi başı gidiş dönüş 4000TL , {personCount} kişi toplam : {personCount * 4000}TL");
                 Console.Write("Hangi yolu tercih edersiniz ? ( 1 / 2 ) : ");
-                travelDecision = Console.ReadLine(); 
+                travelDecision = Console.ReadLine();
                 if (travelDecision != "1" && travelDecision != "2")
                 {
                     if (counter2 == 2)
@@ -93,7 +93,7 @@ namespace ConsoleApp
 
                     Console.WriteLine("Lütfen '1' veya '2' seçeneklerinden birini seçiniz");
                     counter2++;
-                    
+
                 }
             } while (travelDecision != "1" && travelDecision != "2");
 
@@ -108,8 +108,8 @@ namespace ConsoleApp
                     break;
             }
 
-            Console.WriteLine("Yanıtlarınız için teşekkürler,");
-            Console.WriteLine($"Toplam ödemeniz gereken ücretiniz : {personCount * (pricePerPerson + travelPrice)}TL");
+            Console.WriteLine("TEŞEKKÜR EDERİZ,");
+            Console.WriteLine($"Toplam ödemeniz gereken ücret : {personCount * (pricePerPerson + travelPrice)}TL");
             Console.WriteLine("İyi eğlenceler dileriz !");
 
         }
