db0aa5937e061bbdf41683a782b98a89044f7a06
Umut Aydın <169164758+aydinumu@users.noreply.github.com>
2024-08-23 20:18:19 +0300

    Create array.cs

---
+++ array.cs
@@ -0,0 +1,47 @@
+/*
+Tasks:
+Define an array that will hold 10 integer values.
+Fill the elements of this array using a for loop, then print them to the console using a foreach loop.
+Add a new value, taken from the user, to this array as the 11th element.
+Print the array elements in descending order.
+*/
+
+// Note: A separate README file is not used as all tasks are completed within a single file.
+
+using System.Collections.Immutable;
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            int[] myArr = new int[10]; // Creating array
+
+            // Assigning values with a for loop
+            for (int i = 0; i < myArr.Length; i++)
+            {
+                Random rnd = new Random(); // Assigning random number
+                myArr[i] = rnd.Next(0, 100);
+            }
+
+            // Printing all the items in console
+            foreach (int item in myArr)
+            {
+                Console.WriteLine(item);
+            }
+          
+            // Resizing myArray because it was predefined as 10
+            Array.Resize(ref myArr, 11); 
+            Console.Write("Please enter a number to add to the list : ");
+            myArr[myArr.Length - 1] = Convert.ToInt32(Console.ReadLine());
+
+            // Sorting in ascending order
+            Array.Sort(myArr);
+            // Sorting in descending order and printing in console with a for loop
+            for (int i = myArr.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine(myArr[i]);
+            }
+        }
+    }
+}
