13ac763658f9fe9b47492a318570c385a800f95b
Umut Aydın <169164758+aydinumu@users.noreply.github.com>
2024-09-19 22:41:10 +0300

    Update HomeController.cs

--- ASP.NET-Core-Empty/Controllers/HomeController.cs
+++ ASP.NET-Core-Empty/Controllers/HomeController.cs
@@ -1,10 +1,15 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ASP.NET_Core_Empty.Controllers
 {
 	public class HomeController : Controller
 	{
 		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult About()
 		{
 			return View();
 		}
